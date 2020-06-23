99e810b1067bdcb2fe3a59799f69e9028555e2a9
PierreMazoue <pierre.mazoue@atos.net>
2020-06-23 11:19:30 +0200

    Icons components

--- src/app/icon/icon.module.ts
+++ src/app/icon/icon.module.ts
@@ -1,12 +1,19 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { IconCloseComponent } from './components/icon/icon-close.component';
+import { IconDeleteComponent } from './components/icon/icon-delete.component';
+import { IconEditComponent } from './components/icon/icon-edit.component';
+import { IconNavComponent } from './components/icon/icon-nav.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [IconDeleteComponent, IconEditComponent, IconNavComponent, IconCloseComponent],
   imports: [
-    CommonModule
-  ]
+    CommonModule,
+    FontAwesomeModule
+  ],
+  exports: [IconDeleteComponent, IconEditComponent, IconNavComponent, IconCloseComponent]
 })
 export class IconModule { }
